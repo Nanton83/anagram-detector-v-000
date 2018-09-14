d9ccabe8ed9d2805484935cc2b56cd63f62a05a1
Nick Romans <nicholas.romans@gmail.com>
2018-09-14 19:08:19 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ end
   
 def match(array_of_anagrams)
   array_of_anagrams.find_all do |word_to_match|
-    if word.split("").sort == word_to_match.split("").sort
+    if @word.split("").sort == word_to_match.split("").sort
   end
   word_to_match
 end 
