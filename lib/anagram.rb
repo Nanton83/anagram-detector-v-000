21e6a4efb2ec4adf10de9a21186044db2edb6c98
Nick Romans <nicholas.romans@gmail.com>
2018-09-14 19:05:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize(word)
 end
   
 def match(array_of_anagrams)
-  array_of_anagrams.collect do |word_to_match|
+  array_of_anagrams.find_all do |word_to_match|
     if word.split("").sort == word_to_match.split("").sort
   end
   word_to_match
