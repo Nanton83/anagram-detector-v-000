925a7f8a68ebc5bda45a76526681362baeb3a6af
Nick Romans <nicholas.romans@gmail.com>
2018-09-14 18:55:43 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,11 @@ def initialize(word)
 end
   
 def match(array_of_anagrams)
+  array_of_anagrams.each do |word_to_match|
+    if @word.split("") == word_to_match.split("")
+    @word
+  end
+end 
 end
 
 end 
